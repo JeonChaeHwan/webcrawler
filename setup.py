a52a91ae1c94575b55710cf653097d392f54d599
JeonChaeHwan <123877184+JeonChaeHwan@users.noreply.github.com>
2024-04-20 16:51:00 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name = "webcrawlers",
+    packages=["myweb"],
+    version="0.0.1",
+    description="oss easily webCrawler",
+    author = "ChaeHwanJeon",
+    url ="https://github.com/JeonChaeHwan/webcrawler",
+    download_url="https://github.com/JeonChaeHwan/webcrawler"
+    install_requires = ["bs4", "requests"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ]
